613229b7bb8a81cbc092f16da07066ae3b989579
Nithin <Nithinthomasnin6468@conestogac.on.ca>
2024-02-23 16:39:40 -0500

    Clear drive

--- Capstone Project/app/src/main/java/com/example/cleardrive/AboutUsFragment.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.cleardrive;
-
-import androidx.fragment.app.Fragment;
-
-public class AboutUsFragment extends Fragment {
-}
--- Capstone Project/app/src/main/java/com/example/cleardrive/HistoryFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.cleardrive;
-
-import androidx.fragment.app.Fragment;
-
-public class HistoryFragment extends Fragment {
-}
